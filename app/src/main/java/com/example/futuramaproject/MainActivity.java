fbc451058deeff05c9de818fac75846d1a144413
Sytyy <sytnykandrey@gmail.com>
2021-08-26 14:07:29 +0300

    Created new Project. Created "Main Menu" layout Implemented "Quit" button

---
+++ app/src/main/java/com/example/futuramaproject/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.futuramaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer futuramaThemeSong;
+
+    @BindView(R.id.quit)
+    Button quit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        futuramaThemeSong = MediaPlayer.create(MainActivity.this, R.raw.futurama_theme_song);
+        futuramaThemeSong.start();
+    }
+
+    public MediaPlayer getFuturamaThemeSong() {
+        return futuramaThemeSong;
+    }
+
+    public void onQuit(View view){
+        finish();
+        System.exit(0);
+    }
+}
